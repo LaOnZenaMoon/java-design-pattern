95762e8a4f27ce6820d3fb43f48f6d326529309b
junlee <laonzenamoon@gmail.com>
2020-10-16 11:14:56 +0900

    - Thread 관련 테스트 => 임계영역(Critical Section) 관련 테스트중...

---
+++ thread/src/main/java/lozm/ThreadBlocking.java
@@ -0,0 +1,38 @@
+package lozm;
+
+import java.util.concurrent.*;
+
+public class ThreadBlocking {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("Thread1 start");
+                Thread.sleep(3000);
+                System.out.println("Thread1 end");
+                return "success";
+            }
+        });
+
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Thread2 start");
+                    String result = future.get();
+                    System.out.println("result = " + result);
+                    System.out.println("Thread2 end");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        executorService.shutdown();
+    }
+
+}
