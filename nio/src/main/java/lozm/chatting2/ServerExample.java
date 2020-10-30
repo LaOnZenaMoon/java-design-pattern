3d9ed16e976231f5eaeac05f88f581a8166d221a
junlee <laonzenamoon@gmail.com>
2020-10-30 14:56:30 +0900

    - NIO 개발 및 테스트 > Selector & Non-blocking 구조 => 채팅 클라이언트 개발중

--- nio/src/main/java/lozm/chatting2/ServerExample.java
+++ nio/src/main/java/lozm/chatting2/ServerExample.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -79,10 +81,44 @@ public class ServerExample extends Application {
         thread.start();
     };
 
-    void stopServer() {};
+    void stopServer() {
+        try {
+            Iterator<Client> iterator = connections.iterator();
+            while(iterator.hasNext()) {
+                Client client = iterator.next();
+                client.socketChannel.close();
+                iterator.remove();
+            }
+
+            if(serverSocketChannel != null && serverSocketChannel.isOpen()) serverSocketChannel.close();
+
+            if(selector != null && selector.isOpen()) selector.close();
+
+            Platform.runLater(() -> {
+                displayText("[서버 종료]");
+                btnStartStop.setText("start");
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
 
     void accept(SelectionKey selectionKey) {
         //연결 수락 코드
+        try {
+            ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+            SocketChannel socketChannel = serverSocketChannel.accept();
+
+            String message = "[연결 수락:" + socketChannel.getRemoteAddress() + ": " + Thread.currentThread().getName() + "]";
+            Platform.runLater(() -> displayText(message));
+
+            Client client = new Client(socketChannel);
+            connections.add(client);
+
+            Platform.runLater(() -> displayText("[연결 개수: " + connections.size() + "]"));
+        } catch (Exception e) {
+            if(serverSocketChannel.isOpen()) stopServer();
+        }
     };
 
     @Override
@@ -90,9 +126,31 @@ public class ServerExample extends Application {
 
     }
 
+    //데이터 통신 코드
     class Client {
-        //데이터 통신 코드
-    }
 
+        SocketChannel socketChannel;
+        String sendData; //클라이언트로 보낼 데이터를 저장하는 필드
+
+
+        Client(SocketChannel socketChannel) throws IOException {
+            this.socketChannel = socketChannel;
+            this.socketChannel.configureBlocking(false);
+
+            //읽기 작업 유형으로 Selector 에 등록시킴
+            SelectionKey selectionKey = this.socketChannel.register(selector, SelectionKey.OP_READ);
+
+            //SelectionKey 에 자기 자신을 첨부 객체로 저장
+            selectionKey.attach(this);
+        }
+
+        void receive(SelectionKey selectionKey) {
+            //데이터 받기 코드
+        }
+
+        void send(SelectionKey selectionKey) {
+            //데이터 전송 코드
+        }
+    }
 
 }
