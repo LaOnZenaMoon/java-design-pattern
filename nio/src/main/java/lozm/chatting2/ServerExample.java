6969ef7684147d1e09c81d2c257f45ea65b61acd
junlee <laonzenamoon@gmail.com>
2020-10-30 14:20:08 +0900

    - NIO 개발 및 테스트 > Selector & Non-blocking 구조 => 채팅 클라이언트 개발중

--- nio/src/main/java/lozm/chatting2/ServerExample.java
+++ nio/src/main/java/lozm/chatting2/ServerExample.java
@@ -3,8 +3,10 @@ package lozm.chatting2;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.util.List;
 import java.util.Vector;
 
 public class ServerExample extends Application {
@@ -13,10 +15,21 @@ public class ServerExample extends Application {
     ServerSocketChannel serverSocketChannel;
     List<Client> connections = new Vector<Client>();
 
+    void startServer() {};
+
+    void stopServer() {};
+
+    void accept(SelectionKey selectionKey) {
+        //연결 수락 코드
+    };
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+    }
+
+    class Client {
+        //데이터 통신 코드
     }
 
 }
