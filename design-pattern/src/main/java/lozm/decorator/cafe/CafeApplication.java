239491bf6e19588f268ed2bae83297c6d6f6d19b
junlee <laonzenamoon@gmail.com>
2020-11-19 16:30:38 +0900

    feat($compile): 데코레이터 객체 실행 추가

--- design-pattern/src/main/java/lozm/decorator/cafe/CafeApplication.java
+++ design-pattern/src/main/java/lozm/decorator/cafe/CafeApplication.java
@@ -2,8 +2,6 @@ package lozm.decorator.cafe;
 
 public class CafeApplication {
 
-
-
     public static void main(String[] args) {
         Beverage espresso = new Espresso();
         System.out.println("espresso = " + espresso.getDescription() + ", $ " + espresso.cost());
@@ -14,6 +12,10 @@ public class CafeApplication {
         houseBlend = new Whip(houseBlend);
         houseBlend = new Whip(houseBlend);
         System.out.println("houseBlend = " + houseBlend.getDescription() + ", $ " + houseBlend.cost());
+
+
+        Beverage espresso2 = new Whip(new Mocha(new Espresso()));
+        System.out.println("espresso2 = " + espresso2.getDescription() + ", $ " + espresso2.cost());
     }
 
 }
