363b03d07e45b90965e68abce3cb59544144e55b
junlee <laonzenamoon@gmail.com>
2020-12-18 11:07:48 +0900

    feat($compile): 컴파운드 패턴 연습
    - 추상 팩토리 패턴을 활용

---
+++ design-pattern/src/main/java/lozm/compound/AbstractDuckFactory.java
@@ -0,0 +1,10 @@
+package lozm.compound;
+
+public abstract class AbstractDuckFactory {
+
+    public abstract Quackable createMallardDuck();
+    public abstract Quackable createRedHeadDuck();
+    public abstract Quackable createDuckCall();
+    public abstract Quackable createRubberDuck();
+
+}
